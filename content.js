9da253232828c1c3dbd44ae7635d6886bbd8b1ae
saran <saransathi1010@gmail.com>
2024-05-29 12:53:49 +0530

    new Extension

---
+++ content.js
@@ -0,0 +1,32 @@
+const newProfilePicUrl = 'https://images.pexels.com/photos/3039036/pexels-photo-3039036.jpeg';
+
+function replaceProfilePictures() {
+  const profilePics = document.querySelectorAll('img');
+  const gostprofiles = document.querySelectorAll('.Elevation-0dp')
+  profilePics.forEach(pic => {
+    if (pic.alt.toLowerCase().includes("profile photo") || pic.classList.contains("EntityPhoto-circle-0") || pic.classList.contains("EntityPhoto-circle-3") || pic.classList.contains("EntityPhoto-square-3") || pic.classList.contains("evi-image") ||pic.classList.contains("EntityPhoto-circle-3-ghost-person") || pic.classList.contains("Elevation-0dp") || pic.classList.contains("discover-entity-type-card__image-circle--dash") || pic.classList.contains("ivm-view-attr__ghost-entity") ){
+      console.log('Replacing image:', pic.src, 'with', newProfilePicUrl);
+      pic.src = newProfilePicUrl;
+
+      pic.onload = () => console.log('Image loaded successfully');
+      pic.onerror = () => console.log('Failed to load image');
+    }
+  });
+
+  gostprofiles.forEach(pic => {
+      console.log('Replacing image:', pic.src, 'with', newProfilePicUrl);
+      pic.src = newProfilePicUrl;
+
+      pic.onload = () => console.log('Image loaded successfully');
+      pic.onerror = () => console.log('Failed to load image');
+    }
+//   }
+);
+}
+
+
+
+replaceProfilePictures();
+
+const observer = new MutationObserver(replaceProfilePictures);
+observer.observe(document.body, { childList: true, subtree: true });
