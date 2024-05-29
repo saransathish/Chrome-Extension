12330b7043c9d8f6b70d8d51f91a81c003e282be
saran <saransathi1010@gmail.com>
2024-05-29 13:25:45 +0530

    updates

--- content.js
+++ content.js
@@ -4,7 +4,7 @@ function replaceProfilePictures() {
   const profilePics = document.querySelectorAll('img');
   const gostprofiles = document.querySelectorAll('.Elevation-0dp')
   profilePics.forEach(pic => {
-    if (pic.alt.toLowerCase().includes("profile photo") || pic.classList.contains("EntityPhoto-circle-0") || pic.classList.contains("EntityPhoto-circle-3") || pic.classList.contains("EntityPhoto-square-3") || pic.classList.contains("evi-image") ||pic.classList.contains("EntityPhoto-circle-3-ghost-person") || pic.classList.contains("Elevation-0dp") || pic.classList.contains("discover-entity-type-card__image-circle--dash") || pic.classList.contains("ivm-view-attr__ghost-entity") ){
+    if (pic.alt.toLowerCase().includes("profile photo") || pic.classList.contains("EntityPhoto-circle-0") || pic.classList.contains("EntityPhoto-circle-3") || pic.classList.contains("EntityPhoto-square-3") || pic.classList.contains("EntityPhoto-circle-1") ||pic.classList.contains("EntityPhoto-circle-3-ghost-person")  || pic.classList.contains("discover-entity-type-card__image-circle--dash") || pic.classList.contains("ivm-view-attr__ghost-entity") || pic.classList.contains("ivm-image-view-model__circle-img")  || pic.classList.contains("EntityPhoto-square-1") ){
       console.log('Replacing image:', pic.src, 'with', newProfilePicUrl);
       pic.src = newProfilePicUrl;
 
@@ -13,15 +13,15 @@ function replaceProfilePictures() {
     }
   });
 
-  gostprofiles.forEach(pic => {
-      console.log('Replacing image:', pic.src, 'with', newProfilePicUrl);
-      pic.src = newProfilePicUrl;
+  // gostprofiles.forEach(pic => {
+  //     console.log('Replacing image:', pic.src, 'with', newProfilePicUrl);
+  //     pic.src = newProfilePicUrl;
 
-      pic.onload = () => console.log('Image loaded successfully');
-      pic.onerror = () => console.log('Failed to load image');
-    }
+  //     pic.onload = () => console.log('Image loaded successfully');
+  //     pic.onerror = () => console.log('Failed to load image');
+  //   }
 //   }
-);
+// );
 }
 
 
